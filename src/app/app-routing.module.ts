487e7e623cfc4b7f0f891d8d5c22dddbe9b41f36
anarchy <951202500@qq.com>
2019-07-10 23:49:38 +0800

    init commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { OrderPageComponent } from './order-page/order-page.component';
+import { OrderDetailComponent } from './order-detail/order-detail.component';
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  { path: 'homePage', component: HomePageComponent },
+  { path: 'orderPage', component: OrderPageComponent },
+  { path: 'orderDetail', component: OrderDetailComponent},
+  { path: '',   redirectTo: '/homePage', pathMatch: 'full' },
+];
+
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NgZorroAntdMobileModule } from 'ng-zorro-antd-mobile';
+import { HomePageComponent } from './home-page/home-page.component';
+import { OrderPageComponent } from './order-page/order-page.component';
+import { OrderDetailComponent } from './order-detail/order-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    OrderPageComponent,
+    OrderDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    NgZorroAntdMobileModule
   ],
   providers: [],
   bootstrap: [AppComponent]
